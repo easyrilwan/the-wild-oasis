88fabfbcb7cb957950d39a0f7be4e46a71b2b347
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2024-10-31 18:29:16 +0100

    supabase final commit

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -12,6 +12,10 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://iexviuuvrbrjspfisiga.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+        alt="cabins image"
+      />
     </Row>
   );
 }
