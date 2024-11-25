af38da57e2fa123f97653c8c6e352086e3d831ab
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2024-11-25 19:19:35 +0100

    finally

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -87,6 +87,8 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
+
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -103,9 +105,9 @@ function Toggle({ id }) {
   );
 }
 
-function List({ id, children, close }) {
-  const { openId, position } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+function List({ id, children }) {
+  const { openId, position, close } = useContext(MenusContext);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
 
